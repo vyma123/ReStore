7539357823b91895ff5006081be91ea68e1445c5
Vyma <mavanvy2023@gmail.com>
2023-08-26 18:11:27 +0700

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,9 +1,9 @@
 
-import Catalog from "../../features/catalog/Catalog";
 import { Container, CssBaseline,ThemeProvider, createTheme } from '@mui/material';
 
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 
 
@@ -29,8 +29,7 @@ function App() {
     <CssBaseline /> 
       <Header  darkMode={darkMode}  handleThemeChange={handleThemeChange}/>
       <Container>
-      <Catalog />
-        
+      <Outlet/>
       </Container>
     
     </ThemeProvider>
