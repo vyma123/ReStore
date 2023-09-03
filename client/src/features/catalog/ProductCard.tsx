f95e2b084ec118e213d73b34aaff1d997fa03a9f
Vyma <mavanvy2023@gmail.com>
2023-09-03 09:23:43 +0700

    End of section 7

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,27 +1,21 @@
 import {  Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
-import { useStoreContext } from "../../app/context/StoreContext";
 import { currencyFormat } from "../../app/util/util";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { addBasketItemAsync } from "../basket/basketSlice";
 
 interface Props{
     product:Product;
 }
 
 export default function ProductCard({product}:Props){
-    const [loading, setLoading] = useState(false);
-    const {setBasket} = useStoreContext();
+    const {status} = useAppSelector(state => state.basket);
+    const dispatch = useAppDispatch();
 
-    function handleAddItem(productId: number){
-      setLoading(true);
-      agent.Basket.addItem(productId)
-      .then(basket=> setBasket(basket))
-      .catch(error => console.log(error))
-      .finally(()=> setLoading(false));
-    }
+
+
     return(
         <Card >
           <CardHeader 
@@ -50,7 +44,9 @@ export default function ProductCard({product}:Props){
         </Typography>
       </CardContent>
       <CardActions>
-        <LoadingButton loading={loading} onClick={()=> handleAddItem(product.id)} size="small">Add to cart</LoadingButton>
+        <LoadingButton loading={status.includes('pendingAddItem' + product.id)} 
+        onClick={()=> dispatch(addBasketItemAsync({productId: product.id}))}
+         size="small">Add to cart</LoadingButton>
         <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
       </CardActions>
     </Card>
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,31 +1,28 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Product } from "../../app/models/product"
-import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { useStoreContext } from "../../app/context/StoreContext";
 import { LoadingButton } from "@mui/lab";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import {  addBasketItemAsync, removeBasketItemAsync } from "../basket/basketSlice";
+import { fetchProductAsync, productSelectors } from "./catalogSlice";
 
 export default function ProductDetails() {
-    const {basket, setBasket,removeItem} = useStoreContext();
+    const {basket, status} = useAppSelector(state => state.basket);
+    const dispatch = useAppDispatch();
     const { id } = useParams<{ id: string }>();
-    const [product, setProduct] = useState<Product | null>(null);
-    const [loading, setLoading] = useState(true);
+    const product = useAppSelector(state => productSelectors.selectById(state, id!));
+    const {status: productStatus} = useAppSelector(state => state.catalog);
     const [quantity, setQuantity] = useState(0);
-    const [submitting, setSubmitting] = useState(false);
     const item = basket?.items.find(i => i.productId === product?.id)
 
 
 
     useEffect(() => {
          if(item) setQuantity(item.quantity);
-       id && agent.Catalog.details(parseInt(id))
-            .then(response => setProduct(response)) 
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false));
-    }, [id, item])
+         if(!product && id) dispatch(fetchProductAsync(parseInt(id)))
+    }, [id, item, dispatch, product])
 
     function handleInputChange(event:any){
         if(event.target.value >= 0){
@@ -35,19 +32,12 @@ export default function ProductDetails() {
     }
 
     function handleUpdateCart() {
-        setSubmitting(true);
         if(!item || quantity > item.quantity) {
             const updatedQuantity = item ? quantity - item.quantity: quantity;
-            agent.Basket.addItem(product?.id!, updatedQuantity)
-            .then(basket => setBasket(basket))
-            .catch(error => console.log(error))
-            .finally(()=> setSubmitting(false))
+            dispatch(addBasketItemAsync({productId: product?.id!, quantity: updatedQuantity}))
         } else {
             const updatedQuantity = item.quantity - quantity;
-            agent.Basket.removeItem(product?.id!, updatedQuantity)
-            .then(()=> removeItem(product?.id!, updatedQuantity))
-            .catch(error => console.log(error))
-            .finally(()=> setSubmitting(false));
+            dispatch(removeBasketItemAsync({productId: product?.id!, quantity: updatedQuantity}))
 
 
         }
@@ -55,7 +45,7 @@ export default function ProductDetails() {
 
 
 
-    if (loading) return <LoadingComponent message="Loading product..."/>
+    if (productStatus.includes('pending')) return <LoadingComponent message="Loading product..."/>
 
     if (!product) return <NotFound/>
 
@@ -109,7 +99,7 @@ export default function ProductDetails() {
                         <Grid item xs={6}>
                            <LoadingButton
                            disabled={item?.quantity === quantity || !item && quantity === 0}
-                           loading={submitting}
+                           loading={status.includes('pending')}
                            onClick={handleUpdateCart}
                            sx={{height:'55px'}}
                            color="primary"
